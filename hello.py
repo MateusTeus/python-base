d1c120d65bfa8f9f882bd180834822b9a2971030
Mateus Henrique <mateusteccruz@gmail.com>
2023-10-29 14:48:14 -0300

    SHEBAG e comentario adicionados

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+
+#! /usr/bin/env python3
+#eu quero rodar o env  e passar para o python todas essas variaveis antes de executar meu codigo
+
+# + ! =  SHEBANG = comentario especial q é enviado para o sistema  operacional antes do programa ser executado
+# Este é um comentario que ñ é interpretado pelo python
+
+print("Hello, World!")   # outro comentario
+
